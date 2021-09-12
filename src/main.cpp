6822ad42b1aadbbdea6308b145a72fd5ed0f1636
Sunshine <sunshine@uberspace.net>
2021-09-11 16:21:05 -1000

    only allow single instance of the main window and wrapped apps

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <csignal>
 #include <QApplication>
 #include <QSettings>
 
@@ -6,10 +7,33 @@
 #include "liquidappwindow.hpp"
 #include "mainwindow.hpp"
 
+static QSharedMemory *sharedMemory = NULL;
+
+QString getUserName()
+{
+    QString name = qgetenv("USER");
+
+    if (name.isEmpty())
+        name = qgetenv("USERNAME");
+
+    return name;
+}
+
+static void onSignalHandler(int signum)
+{
+    if (sharedMemory) {
+        delete sharedMemory;
+        sharedMemory = NULL;
+    }
+
+    qDebug() << "Terminated with signal" << signum;
+
+    exit(128 + signum);
+}
+
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-    QAction quitAction;
 
     MainWindow mainWindow;
     LiquidAppWindow liquidAppWindow;
@@ -18,8 +42,33 @@ int main(int argc, char **argv)
     QIcon windowIcon(":/images/" PROG_NAME ".svg");
     app.setWindowIcon(windowIcon);
 
+    // Handle any further termination signals to ensure the
+    // QSharedMemory block is deleted even if the process crashes
+    signal(SIGHUP,  onSignalHandler); // 1
+    signal(SIGINT,  onSignalHandler); // 2
+    signal(SIGQUIT, onSignalHandler); // 3
+    signal(SIGILL,  onSignalHandler); // 4
+    signal(SIGABRT, onSignalHandler); // 6
+    signal(SIGFPE,  onSignalHandler); // 8
+    signal(SIGBUS,  onSignalHandler); // 10
+    signal(SIGSEGV, onSignalHandler); // 11
+    signal(SIGSYS,  onSignalHandler); // 12
+    signal(SIGPIPE, onSignalHandler); // 13
+    signal(SIGALRM, onSignalHandler); // 14
+    signal(SIGTERM, onSignalHandler); // 15
+    signal(SIGXCPU, onSignalHandler); // 24
+    signal(SIGXFSZ, onSignalHandler); // 25
+
     // Process arguments
     if (argc < 2) {
+        // // Allow only one instance
+        sharedMemory = new QSharedMemory(getUserName() + "_Liquid");
+        if (!sharedMemory->create(4, QSharedMemory::ReadOnly)) {
+            delete sharedMemory;
+            qDebug() << "Only one instance of Liquid is allowed";
+            exit(-42);
+        }
+
         mainWindow.show();
     } else  { // App name provided
         QString liquidAppName(argv[1]);
@@ -36,6 +85,14 @@ init:
                                                    nullptr);
         // Attempt to load app settings from a config file
         if (tempAppSettings->contains(SETTINGS_KEY_URL)) {
+            // // Allow only one instance
+            sharedMemory = new QSharedMemory(getUserName() + "_Liquid_app_" + liquidAppName);
+            if (!sharedMemory->create(4, QSharedMemory::ReadOnly)) {
+                delete sharedMemory;
+                qDebug() << "Only one instance of Liquid app is allowed";
+                exit(-42);
+            }
+
             // Found existing liquid app settings file, show it
             liquidAppWindow.runLiquidApp(&liquidAppName);
 
@@ -46,11 +103,10 @@ init:
 
             // Reveal Liquid app creation dialog
             liquidAppCreateEditDialog.show();
-            switch (liquidAppCreateEditDialog.exec())
-            {
+            switch (liquidAppCreateEditDialog.exec()) {
                 case QDialog::Rejected:
                     // Exit the program
-                    return 0;
+                    exit(0);
                 break;
 
                 case QDialog::Accepted:
@@ -68,5 +124,10 @@ init:
         }
     }
 
-    return app.exec();
+    int ret = app.exec();
+
+    delete sharedMemory;
+    sharedMemory = NULL;
+
+    return ret;
 }
